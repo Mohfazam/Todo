96fb3edbeb621efd227bf88e825abbc16b451038
Mohfazam <mohd.sarwar.code@gmail.com>
2024-09-29 04:00:58 +0530

    UI with tasks design done

---
+++ app.js
@@ -0,0 +1,7 @@
+let inputelement = document.querySelector(".input");
+let button = document.querySelector(".add-btn");
+
+button.addEventListener("click", function(){
+    let inputtext = inputelement.value;
+    console.log(inputtext);
+})
